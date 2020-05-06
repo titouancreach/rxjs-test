00cba023b2eb77e66b8a1d4b7de699f9be3013cf
Titouan CREACH <titouan.creach@gmail.com>
2020-05-06 23:17:09 +0200

    start making shit

--- app/App.vue
+++ app/App.vue
@@ -1,24 +1,97 @@
 <template>
-  <h1>Hello Vite + Vue 3!</h1>
-  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
-  <p>
-    <span>Count is: {{ count }}</span>
-    <button @click="count++">increment</button>
-  </p>
+  <div class="absolute-fill">
+    <div
+      v-for="word in wordsList"
+      :key="word.value"
+      :style="{
+        color: 'yellow',
+        position: 'absolute',
+        right: word.x,
+        top: word.y,
+      }"
+    >
+      {{ word.value }}
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  data: () => ({ count: 0 })
+<script lang="ts">
+import { interval, fromEvent } from "rxjs";
+import { throttle, map, flatMap, distinctUntilChanged } from "rxjs/operators";
+
+async function* nextWord() {
+  while (true) {
+    const response = await fetch(
+      "https://random-word-api.herokuapp.com/word?number=100"
+    );
+    const words = await response.json();
+    yield* words;
+  }
 }
+
+export default {
+  data: () => ({
+    iter: nextWord(),
+    wordsList: [],
+    timer$: interval(1000),
+    keyDown$: fromEvent(document, "keydown").pipe(
+      (distinctUntilChanged(
+        (x: KeyboardEvent, y: KeyboardEvent) => x.type === y.type
+      ),
+      map((x: KeyboardEvent) => x.key))
+    ),
+  }),
+
+  async mounted() {
+    const word = await this.iter.next();
+    this.wordsList = [
+      ...this.wordsList,
+      {
+        value: word.value,
+        x: 0,
+        y: 200,
+      },
+    ];
+
+    this.timer$.subscribe(() => {
+      for (const word of this.wordsList) {
+        word.x += 20;
+      }
+    });
+
+    this.timer$
+      .pipe(
+        throttle(() => interval(4000)),
+        flatMap(async () => {
+          const word = await this.iter.next();
+          return word.value;
+        })
+      )
+      .subscribe((word) => {
+        this.wordsList = [
+          ...this.wordsList,
+          {
+            value: word,
+            x: 0,
+            y: Math.floor(Math.random() * window.innerHeight),
+          },
+        ];
+      });
+
+    this.keyDown$.subscribe((x) => {
+      console.log(x);
+    });
+  },
+};
 </script>
 
 <style scoped>
-h1 {
-  color: #4fc08d;
-}
-
-h1, p {
-  font-family: Arial, Helvetica, sans-serif;
+.absolute-fill {
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: blue;
+  position: absolute;
 }
 </style>
